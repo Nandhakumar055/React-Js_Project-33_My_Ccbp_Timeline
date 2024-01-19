8631adcc97c89ff8e7339a52f98e724602158a40
Nandhakumar055 <Nandhakumar055.githup.com>
2024-01-19 18:11:02 +0000

    first commit

---
+++ src/components/TimelineView/styledComponents.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components/macro'
+
+export const TimelineContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-height: 100vh;
+  background-image: linear-gradient(to right, white, rgb(229, 186, 235));
+`
+
+export const ResponsiveContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1110px;
+  height: 100vh;
+`
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`
+
+export const Heading = styled.h1`
+  text-align: center;
+  color: rgb(69, 5, 71);
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 2.7;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+`
+
+export const CCBPHeading = styled.span`
+  color: rgb(148, 0, 153);
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 24px;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+`
